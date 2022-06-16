cb9479c932aad8404b012423f485aa2adc1b5d8c
Alex Collino <arcollino@gmail.com>
2022-06-16 14:03:06 -0700

    add initial error-handling

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect
 from stories import Story, get_stories
 
 app = Flask(__name__)
-
-story_example = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
+app.secret_key = "to be changed if app is no longer public on github"
 
 story_list = get_stories()
 
@@ -18,10 +13,26 @@ def home_page():
 
 @app.route("/prompts/<chosen_story>")
 def story_prompts(chosen_story):
-    return render_template("prompts.html", story=story_list.get(chosen_story))
+    if chosen_story not in story_list:
+        flash("You requested a story that doesn't exist!")
+        return redirect("/")
+    else:
+        return render_template("prompts.html", story=story_list.get(chosen_story))
 
 @app.route("/story/<chosen_story>")
 def show_story(chosen_story):
-    user_input = request.args
-    generated_story = story_list.get(chosen_story).generate_multi(user_input)
-    return render_template("story.html", story=generated_story)
+    if chosen_story not in story_list:
+        flash("You requested a story that doesn't exist!")
+        return redirect("/")
+    elif len(request.args) == 0 or len(list(request.args.values())) == 0:
+            flash("You did not fill in all of the prompts!")
+            return redirect(f"/prompts/{chosen_story}")
+    else:
+        generated_story = story_list.get(chosen_story).generate_multi(request.args)
+        return render_template("story.html", story=generated_story)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    flash("Something went wrong there, so you've been returned to the main page.")
+    flash(f"This was the error: {e}")
+    return redirect("/")
